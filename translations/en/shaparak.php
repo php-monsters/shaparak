9bcb4227da985bc2fc1530545c0c680122f7707f
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-01-29 23:51:36 +0330

    added inquiry messages

--- translations/en/shaparak.php
+++ translations/en/shaparak.php
@@ -2,7 +2,7 @@
 
 return [
     'canceled_by_user'             => 'canceled_by_user',
-    'could_not_request_payment'    => 'could_not_request_payment',
+    'could_not_request_token'      => 'could_not_request_token',
     'could_not_verify_transaction' => 'could_not_verify_transaction',
     'could_not_inquiry_payment'    => 'could_not_inquiry_payment',
     'could_not_settle_payment'     => 'could_not_settle_payment',
@@ -10,7 +10,9 @@ return [
     'gate_not_ready'               => 'gate_not_ready',
     'goto_gate'                    => 'goto_gate',
     'invalid_response'             => 'invalid_response',
-    'refund_failed'                => 'refund_failed',
+    'token_failed'                 => 'token_failed',
     'verify_failed'                => 'verify_failed',
+    'inquiry_failed'               => 'inquiry_failed',
     'settle_failed'                => 'settle_failed',
+    'refund_failed'                => 'refund_failed',
 ];
